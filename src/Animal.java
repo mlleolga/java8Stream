990469044326beeee5227b09b37e09e02e3fd358
saas@kindgeek.com <saas@kindgeek.com>
2017-02-18 12:43:00 +0200

    olga

---
+++ src/Animal.java
@@ -0,0 +1,36 @@
+/**
+ * Created by root on 18.02.17.
+ */
+public class Animal {
+
+    private String name;
+    private int age;
+    private int age2;
+
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+
+    public Animal() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
